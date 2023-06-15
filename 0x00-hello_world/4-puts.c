6571dbf44135e5fce4f23f326a3790300d7924be
Collins O. Birundu <cbirundu23@gmail.com>
2023-06-15 05:46:50 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
-/* A program that  prints exactly "Programming is like building a multilingual puzzle", followed by a new line.*/
-
+/* A program that  prints line with puts function*/
 #include <stdio.h>
 int main(void)
 {
